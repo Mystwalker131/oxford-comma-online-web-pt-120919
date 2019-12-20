00a252600265b03a094fd31a104ca7ac7de756a0
Benito Galvez <mystwalker131@gmail.com>
2019-12-20 01:01:02 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,11 +1,11 @@
+#write a method oxford_comma
+#takes in an argument of (array), using oxford_comma
+#converts (array)into a string using oxford_comma
 
 
 
 
 
-
-
-
-def oxford_comma(array) #write a method oxford_comma
+def oxford_comma(array) #write a method ,oxford_comma
 
 end
